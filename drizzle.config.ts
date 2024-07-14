7276163727a5048476f7e322969de545c4a38b13
mohamed <mohamedabbj@gmail.com>
2024-07-14 14:27:07 -0400

    fix build fail with drizzle

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-  schema: "./drizzle/schema.ts",
-  driver: "pg",
-  dbCredentials: {
-    connectionString: process.env.POSTGRES_URL!,
-  },
-});
