8fec27692b5419547357a3b25ecdea2158d13da0
mohamed <mohamedabbj@gmail.com>
2024-07-18 23:36:51 -0400

    adding icon and fixing layout

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import Image from "next/image";
+import ScissorsIcon from "../../../public/logoScissors.svg";
+
 export default function SignIn() {
   const signInOptions = [
     {
@@ -17,13 +20,16 @@ export default function SignIn() {
   ];
 
   return (
-    <div className="flex items-center justify-center w-full h-lvh outline [&_div]:border border-black">
+    <div className="flex items-center justify-center w-full h-lvh outline [&_div]:flex [&_div]:flex-col">
       <div className="outline flex-col flex items-center py-36 px-52 rounded-lg">
+        <Image width={17} src={ScissorsIcon} alt="scissorsIcon" />
         <p>Welcome to pndek.xyz</p>
         <p>Start creating short links with Supercharge</p>
         <div>
           {signInOptions.map((element) => (
-            <button key={element.id}>{element.optionName}</button>
+            <button
+              key={element.id}
+            >{`Sign in with ${element.optionName}`}</button>
           ))}
         </div>
       </div>
