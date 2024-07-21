bf3c441c2738764365aff90750de0a5c978eca32
mohamed <mohamedabbj@gmail.com>
2024-07-21 15:12:50 -0400

    adding userfetching data to google api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 "use client";
 import { Hero } from "@/components/hero";
 import { Navbar } from "@/components/navbar";
+import googleSignIn from "@/utils/google-sign-in";
 import Image from "next/image";
 import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const currentURL = window.location.href;
+    const params = new URLSearchParams(currentURL);
+    const accessToken = params.get("access_token");
+
+    if (accessToken) {
+      googleSignIn({ mode: "checkUserIdentity", acessToken: accessToken });
+    }
+  }, []);
+
   return (
     <>
       <div className="animate-changing-colors">
--- src/utils/google-sign-in.ts
+++ src/utils/google-sign-in.ts
@@ -1,9 +1,21 @@
 "use server";
 
-const googleSignIn = () => {
-  const googleOauth = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.profile&include_granted_scopes=true&response_type=token&state=state_parameter_passthrough_value&redirect_uri=http://localhost:3000/&client_id=399456137042-5l9ent302j86l5r0l643fodkc0dakogk.apps.googleusercontent.com
- `;
+const googleSignIn = (props: { mode: string; acessToken?: string }) => {
+  const checkUserIdentity = async () => {
+    const getUserData = await fetch(
+      `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${props.acessToken}`
+    );
+    console.log(await getUserData.json());
+  };
 
-  return googleOauth;
+  if (props.mode == "userLogin") {
+    const googleOauth = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&include_granted_scopes=true&response_type=token&state=state_parameter_passthrough_value&redirect_uri=http://localhost:3000/&client_id=399456137042-5l9ent302j86l5r0l643fodkc0dakogk.apps.googleusercontent.com
+   `;
+    return googleOauth;
+  }
+  if (props.mode == "checkUserIdentity") {
+    checkUserIdentity();
+  }
 };
+
 export default googleSignIn;
